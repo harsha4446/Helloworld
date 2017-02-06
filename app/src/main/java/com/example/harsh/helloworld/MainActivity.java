1b728c2217504582b0f43dc309012cea3d62508d
Harsha Bandi <harsha44467@gmail.com>
2017-02-06 22:39:16 +0530

    Commit check

--- app/src/main/java/com/example/harsh/helloworld/MainActivity.java
+++ app/src/main/java/com/example/harsh/helloworld/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.harsh.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button b;
+    TextView box;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b=(Button)findViewById(R.id.button);
+        box=(TextView) findViewById(R.id.textView);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                box.setText("Clicked");
+            }
+        });
     }
 }
